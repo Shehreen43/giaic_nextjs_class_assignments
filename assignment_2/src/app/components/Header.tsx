4713ceac6150c2a6254897bd80d4a0a1a40ca47d
Shehreen <160480886+Shehreen43@users.noreply.github.com>
2024-10-26 20:14:24 +0500

    ...

---
+++ assignment_2/src/app/components/Header.tsx
@@ -0,0 +1,44 @@
+// import Link from 'next/link';
+
+// export default function Header() {
+//   return (
+//     <header className="flex justify-between items-center py-4">
+//       <div className="flex items-center">
+//         <span className="font-bold text-xl mr-4">BrandName</span>
+//        </div>
+//       <nav className="space-x-4">
+//         <Link href="/">Home</Link>
+//         <Link href="/">Product</Link>
+//         <Link href="/">Pricing</Link>
+//         <Link href="/">Contact</Link>
+//       </nav>
+//       <div className="space-x-4">
+//         <button className="text-gray-300">Login</button>
+//         <button className="bg-blue-500 px-4 py-2 rounded text-white">JOIN US</button>
+//       </div>
+//     </header>
+//   );
+// }
+// // const styles = {
+//   navbar: {
+//     backgroundColor: '#f4f4f4',
+//     padding: '10px',
+//     marginBottom: '20px',
+//   },
+//   navList: {
+//     listStyleType: 'none',
+//     display: 'flex',
+//     gap: '20px',
+//   },
+//   navItem: {
+//     fontSize: '18px',
+//   },
+
+export default function Header(){
+
+    return (
+       <header className='w-[1322px] bg-inherit h'>
+       
+       </header>
+    )
+}
