c3a24ab9ff9f4b381b7e4f1c2e76142ab97bd54d
Shehreen <160480886+Shehreen43@users.noreply.github.com>
2024-11-09 11:15:40 +0500

    ...

---
+++ assignment_3/src/app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+
+
+const Navbar = () => {
+  return (
+    <div className='w-[1920px] h-[92px] py-4 px[220px] bg-[#043873]'>
+        <div className='w-[191px] h-[34px]'>
+
+        </div>
+    </div>
+  )
+}
+export default Navbar;
