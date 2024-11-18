803a7e0a4e81d82a80e37677010009bf125bd9bc
Shehreen <160480886+Shehreen43@users.noreply.github.com>
2024-11-18 10:50:40 +0500

    ....

---
+++ assignment_4/e-commerce-website/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import TopHeader from "./components/TopHeader";
+import Navbar from "./components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+    <TopHeader />
+    <Navbar />
+    </>
+  )}
