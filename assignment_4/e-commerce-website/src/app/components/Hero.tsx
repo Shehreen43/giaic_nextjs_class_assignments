339acf58e7b490d82b4a066a888723ec8d314cb9
Shehreen <160480886+Shehreen43@users.noreply.github.com>
2024-11-19 10:17:38 +0500

    ...

---
+++ assignment_4/e-commerce-website/src/app/components/Hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero(){
+    return (
+        <>
+        <main className="absolute w-[1440px] h-[663px] top-[134px] gap-0 bg-[#F0F0F0]">
+        
+        </main>
+        </>
+    )
+}
--- assignment_4/e-commerce-website/src/app/page.tsx
+++ assignment_4/e-commerce-website/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import TopHeader from "./components/TopHeader";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <>
     <TopHeader />
     <Navbar />
+    <Hero />
     </>
   )}
